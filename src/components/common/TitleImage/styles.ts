a123774b98ffbf463777662b291dbf9c6ce3ae7b
Jalila <abdessamadjalila@gmail.com>
2020-11-26 16:22:53 +0100

    Experience Title component

--- src/components/common/TitleImage/styles.ts
+++ src/components/common/TitleImage/styles.ts
@@ -1,21 +1,44 @@
 import { makeStyles, Theme } from '@material-ui/core/styles';
+import { size } from 'lodash';
 
 export default makeStyles<Theme, { color: string; size?: number; font?: string; width?: number }>(() => ({
   container: {
     display: 'flex',
-    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
     position: 'relative',
+    width: '100%',
+    height: 80,
+    backgroundColor: '#FFFFFF',
+  },
+  containerPosition: {
+    justifyContent: 'center',
+  },
+  containerPositionWithNumber: {
+    paddingLeft: 27,
+    paddingRigth: 10,
+    justifyContent: 'space-between',
   },
   imageContainer: {
     height: 'auto',
   },
   image: {
-    width: (props) => props.width || 320,
+    width: (props) => props.width || 68,
     height: 'auto',
+  },
+  number: {
+    position: 'absolute',
+    top: 20,
+    right: 35,
+    fontSize: (props) => props.size || 32,
+    fontFamily: (props) => props.font || 'Ocean',
+    color: (props) => props.color || '#223A7A',
   },
   position: {
     position: 'absolute',
+  },
+  positionWithNumber: {
+    position: 'relative',
+    paddingleft: 27,
+    textTransform: 'capitalize',
   },
 }));
