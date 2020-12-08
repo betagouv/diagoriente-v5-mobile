4f79d98f68f12a25825d2d1f4de6fb7ba49c592c
Jalila <abdessamadjalila@gmail.com>
2020-12-08 15:04:13 +0100

    Select Theme component

---
+++ src/components/inputs/SelectTheme/SelectTheme.tsx
@@ -0,0 +1,147 @@
+import React, { useEffect, useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import arrow from 'assets/images/Arrow.png';
+import classNames from 'utils/classNames';
+import useStyles from './styles';
+import Avatar from 'components/common/AvatarTheme/AvatarTheme';
+import arrowClose from 'assets/svg/arrowClose.svg';
+import Dialog from '@material-ui/core/Dialog';
+import Divider from '@material-ui/core/Divider';
+import Slide from '@material-ui/core/Slide';
+import { TransitionProps } from '@material-ui/core/transitions';
+import { useDidMount } from 'common/hooks/useLifeCycle';
+
+interface Props {
+  avatarsTab: any[];
+  selectedTheme: any;
+  showAvatar: (data: any) => void;
+}
+
+const SelectTheme = ({ avatarsTab, selectedTheme, showAvatar }: Props) => {
+  const classes = useStyles();
+  const closeTitle = 'Thèmes :';
+  const [defaultTheme, setDefaultTheme] = useState('');
+  const [theme, setTheme] = React.useState(defaultTheme);
+  const [open, setOpen] = React.useState(false);
+
+  useEffect(() => {
+    if (selectedTheme?.id) {
+      for (let i = 0; i < avatarsTab.length; i += 1) {
+        if (selectedTheme?.id === avatarsTab[i].id) setDefaultTheme(avatarsTab[i].title);
+      }
+    } else setDefaultTheme(closeTitle);
+  });
+
+  const onOpenSelect = () => {
+    setOpen(true);
+  };
+
+  const onAvatarSelect = (option: any) => {
+    showAvatar(option);
+    setTheme(option.title);
+    setOpen(false);
+  };
+  const handleDialogClose = () => {
+    setOpen(false);
+  };
+
+  const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & { children?: React.ReactElement },
+    ref: React.Ref<unknown>,
+  ) {
+    return <Slide direction="right" ref={ref} {...props} />;
+  });
+  /*  const TransitionClose = React.forwardRef(function Transition(
+    props: TransitionProps & { children?: React.ReactElement },
+    ref: React.Ref<unknown>,
+  ) {
+    return <Slide direction="left" ref={ref} {...props} />;
+  }); */
+
+  return (
+    <>
+      <TextField
+        label=""
+        value={theme ? theme : defaultTheme}
+        variant="outlined"
+        className={
+          !theme || theme === '' || theme === 'Thèmes :' ? classes.selectContainer : classes.selectedThemeContainer
+        }
+        InputProps={{
+          endAdornment: (
+            <div className={classes.arrowContainer}>
+              <img src={arrow} alt="arrow" className={classes.arrow} />
+            </div>
+          ),
+
+          startAdornment: {
+            ...(theme !== 'Thèmes :' ? (
+              <Avatar
+                size={29}
+                className={classes.avatarCircleSelected}
+                squareContainerClassName={classes.squareContainerClassName}
+                theme
+              >
+                <img
+                  src={selectedTheme?.resources?.icon}
+                  alt=""
+                  className={classNames(classes.avatarStyle, classes.selectedImg)}
+                />
+              </Avatar>
+            ) : (
+              <Avatar size={0} className={classes.avatarCircleThemes} />
+            )),
+          },
+        }}
+        onClick={onOpenSelect}
+      ></TextField>
+      <Dialog
+        fullScreen
+        /* onExiting={() => TransitionClose} */
+        open={open}
+        /* TransitionComponent={Transition} */
+        style={{ zIndex: 99999 }}
+      >
+        <MenuItem
+          key={closeTitle}
+          value={closeTitle}
+          className={classes.closeThemeModal}
+          onClick={() => handleDialogClose()}
+        >
+          <div className={classes.itemContainer}>
+            <div className={classes.closeModelContainer}>
+              <img src={arrowClose} alt="arrowClose" className={classes.arrowClose} />
+              <span className={classes.closeModelLabel}> {closeTitle} </span>
+            </div>
+          </div>
+        </MenuItem>
+        <Divider />
+        {avatarsTab.map((option) => (
+          <MenuItem key={option.title} value={selectedTheme} className={classes.item}>
+            <div className={classes.itemContainer}>
+              <Avatar
+                title={option.title}
+                size={62}
+                titleClassName={selectedTheme?.id === option.id ? classes.textSelected : classes.themeTitle}
+                className={classes.avatarCircle}
+                onClick={() => onAvatarSelect(option)}
+                avatarCircleBackground={selectedTheme?.id === option.id ? 'rgba(122, 230, 255, 0.2)' : ''}
+                squareContainerClassName={classes.squareContainerClassName}
+                theme
+              >
+                <img
+                  src={option.resources?.icon}
+                  alt=""
+                  className={classNames(classes.avatarStyle, selectedTheme?.id === option.id && classes.selectedImg)}
+                />
+              </Avatar>
+              <Divider />
+            </div>
+          </MenuItem>
+        ))}
+      </Dialog>
+    </>
+  );
+};
+export default SelectTheme;
---
+++ src/components/inputs/SelectTheme/styles.ts
@@ -0,0 +1,180 @@
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import { calculateBorderBoxPath } from 'html2canvas/dist/types/render/bound-curves';
+
+export default makeStyles((theme: Theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      width: 500,
+    },
+  },
+  select: {
+    padding: '0px',
+    width: '100%',
+  },
+  closeThemeModal: {
+    height: 82,
+    paddingLeft: 70,
+  },
+  closeModelContainer: {
+    minHeight: 26,
+    display: 'flex',
+    alignItems: 'flex-end',
+  },
+  arrowClose: {
+    width: 12,
+    height: 19,
+  },
+  closeModelLabel: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: '26px',
+    color: '#00B2DB',
+    marginLeft: 20,
+  },
+  arrowContainer: {
+    background: '#00CFFF',
+    border: '1px solid #D1D5DE',
+    boxSizing: 'border-box',
+    borderRadius: '0px 5px 5px 0px',
+    width: 42,
+    height: 45,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  arrow: {
+    width: 14,
+    height: 8,
+  },
+  rotatedArrow: {
+    transform: 'rotate(180deg)',
+  },
+  selectContainer: {
+    minWidth: 290,
+    height: 45,
+    display: 'flex',
+    background: '#FFFFFF',
+    border: '1px solid #D1D5DE',
+    boxSizing: 'border-box',
+    borderRadius: '5px 0px 0px 5px',
+    '& div': {
+      display: 'flex',
+      height: 45,
+      alignItems: 'center',
+      fontWeight: 'bold',
+      fontSize: 14,
+      lineHeight: '20px',
+      bottom: 1,
+    },
+    '& fieldset': {
+      border: 'none',
+      color: 'black',
+    },
+    [theme.breakpoints.down(420)]: {
+      width: 230,
+    },
+    '& input': {
+      width: '85%',
+    },
+  },
+
+  selectedThemeContainer: {
+    minWidth: 290,
+    height: 45,
+    display: 'flex',
+    background: '#FFFFFF',
+    border: '1px solid #D1D5DE',
+    boxSizing: 'border-box',
+    borderRadius: '5px 0px 0px 5px',
+    '& div': {
+      display: 'flex',
+      height: 45,
+      alignItems: 'center',
+      fontWeight: 'bold',
+      fontSize: 14,
+      lineHeight: '20px',
+      bottom: 1,
+    },
+    '& fieldset': {
+      border: 'none',
+      color: 'black',
+    },
+    [theme.breakpoints.down(420)]: {
+      width: 230,
+    },
+    '& input': {
+      color: '#00B2DB',
+      width: '75% !important',
+    },
+  },
+
+  /*   selectedTheme: {
+    '& input': {
+      color: '#00B2DB',
+      width: '75%',
+    },
+  },
+ */
+  item: {
+    /* paddingRight: '12px !important', */
+    padding: '0px !important',
+    width: '100%',
+  },
+  itemContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+
+  themeTitle: {
+    margin: '0 15px',
+    color: '#424242',
+    fontSize: 14,
+  },
+
+  textSelected: {
+    margin: '0 15px',
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#00B2DB',
+  },
+  avatarCircle: {
+    width: '100%',
+    minWidth: '100%',
+    height: '51px !important',
+    flexDirection: 'row',
+    borderRadius: '0px !important',
+    paddingBottom: '0px !important',
+    paddingLeft: 62,
+    cursor: 'pointer',
+    '&:hover': {
+      background: 'rgba(122, 230, 255, 0.2)',
+    },
+  },
+  avatarCircleSelected: {
+    width: '45px !important',
+  },
+
+  avatarCircleThemes: {
+    width: '15px !important',
+  },
+
+  squareContainerClassName: {
+    width: '30px !important',
+    height: '30px !important',
+  },
+  avatarStyle: {
+    position: 'relative',
+    maxHeight: '100%',
+    maxWidth: '100%',
+  },
+  selectedImg: {
+    '&:not(.nohover):hover': {
+      opacity: 1,
+    },
+  },
+  line: {
+    border: '1px solid #C9C9C7',
+    width: '100%',
+  },
+}));
