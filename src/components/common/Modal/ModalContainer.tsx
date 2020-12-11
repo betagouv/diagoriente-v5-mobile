1f9d44f3307eea9bf6e0b4236962b0d471d4f9ac
Mahdi Hellal <mahdihellal@gmail.com>
2020-12-11 09:32:56 +0100

    delete modal close

--- src/components/common/Modal/ModalContainer.tsx
+++ src/components/common/Modal/ModalContainer.tsx
@@ -39,12 +39,12 @@ const ModalContainer = ({
     >
       <div className={classes.modalContainer}>
         <div className={classes.modal}>
-          {handleClose && (
+          {/*  {handleClose && (
             <div className={classes.header}>
               {title}
               <Reset className={classes.reset} color={colorIcon} label="Fermer" onClick={onReset || handleClose} />
             </div>
-          )}
+          )} */}
           <div className={classes.modalContainerBody}>{children}</div>
         </div>
       </div>
