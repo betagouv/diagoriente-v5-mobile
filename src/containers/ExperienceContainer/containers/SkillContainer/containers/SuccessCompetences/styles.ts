610c7deeeaac4c65751924804ae83b7b239ca80b
Mahdi Hellal <mahdihellal@gmail.com>
2020-12-14 12:48:34 +0100

    buttons box shadow

--- src/containers/ExperienceContainer/containers/SkillContainer/containers/SuccessCompetences/styles.ts
+++ src/containers/ExperienceContainer/containers/SkillContainer/containers/SuccessCompetences/styles.ts
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   root: {
     width: '100%',
-    height: '100%',
+    height: '100vh',
     display: 'flex',
     justifyContent: 'center',
     backgroundColor: '#F3F2F4',
