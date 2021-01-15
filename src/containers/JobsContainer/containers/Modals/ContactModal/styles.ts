7ca1a88203cfd45535353522fc7c94ef5352d313
Mahdi Hellal <mahdihellal@gmail.com>
2021-01-15 14:51:03 +0100

    contact modal

--- src/containers/JobsContainer/containers/Modals/ContactModal/styles.ts
+++ src/containers/JobsContainer/containers/Modals/ContactModal/styles.ts
@@ -1,20 +1,47 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const MAX_WIDTH = 700;
+const MAX_WIDTH = 600;
 const FONT_SIZE = 16;
-const PADDING = '0px 50px 30px 50px';
-export default makeStyles(() => ({
+const PADDING = '0px 45px 30px 45px';
+export default makeStyles((theme) => ({
   root: {
-    padding: PADDING,
+    background: '#FFFFFF',
+    boxSizing: 'border-box',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    overflow: 'auto',
+  },
+  closeFullModelContainer: {
+    width: '100%',
+    height: 80,
     display: 'flex',
-    flexDirection: 'column',
     alignItems: 'center',
+    paddingLeft: 45,
+    borderBottom: '0.5px solid #C9C9C7',
+    [theme.breakpoints.down(415)]: {
+      paddingLeft: 15,
+    },
+  },
+  closeModelLabel: {
+    fontFamily: 'Andika New Basic',
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: '26px',
+    color: '#DB8F00',
+    marginLeft: 20,
   },
 
   maxWidth: {
     display: 'flex',
     flexDirection: 'column',
     maxWidth: MAX_WIDTH,
+    padding: PADDING,
+    [theme.breakpoints.down(415)]: {
+      padding: '0px 15px 30px 15px',
+    },
   },
 
   titleContainer: {
@@ -22,7 +49,7 @@ export default makeStyles(() => ({
     fontFamily: 'ocean',
     marginTop: 25,
     textAlign: 'center',
-    textTransform: "uppercase",
+    textTransform: 'uppercase',
   },
 
   contactContainer: {
@@ -71,13 +98,13 @@ export default makeStyles(() => ({
   textIdeaRoot: {
     display: 'flex',
     color: '#db8f00',
-    width: '100%',
     paddingBottom: 20,
     WebkitTextStroke: '#db8f00',
     WebkitTextStrokeWidth: '0.2px',
   },
   textIdea: {
     paddingLeft: 10,
+    maxWidth: 'fit-content',
   },
   ideaText: {
     fontSize: 12,
@@ -111,6 +138,8 @@ export default makeStyles(() => ({
     fontWeight: 'bold',
     fontSize: FONT_SIZE,
   },
+
+  arrow: { width: '8px !important', height: '13 !important' },
 
   modalContainer: {
     display: 'flex',
