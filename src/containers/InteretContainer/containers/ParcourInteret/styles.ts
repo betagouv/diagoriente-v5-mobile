0125b4c2ab48640869000dc684a7296c76645722
Mahdi Hellal <mahdihellal@gmail.com>
2021-01-20 09:53:01 +0100

    interet style

--- src/containers/InteretContainer/containers/ParcourInteret/styles.ts
+++ src/containers/InteretContainer/containers/ParcourInteret/styles.ts
@@ -208,7 +208,7 @@ export default makeStyles<Theme>((theme) => ({
     textAlign: 'center',
   },
   itemRow: {
-    height: 104,
+    height: 124,
     display: 'flex',
     alignItems: 'center',
   },
