a61af70974474e3161d99b685bf7e0dcf8b7387e
Mahdi Hellal <mahdihellal@gmail.com>
2021-04-16 12:26:37 +0100

    eng question list

--- src/containers/ExperienceContainer/containers/SkillContainer/containers/EngagementActivities/components/QuestionList/QuestionList.tsx
+++ src/containers/ExperienceContainer/containers/SkillContainer/containers/EngagementActivities/components/QuestionList/QuestionList.tsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Question, Option } from 'requests/types';
 import classNames from 'utils/classNames';
-
 import { useQuestions } from 'requests/questions';
-
-import Remove from '@material-ui/icons/RemoveCircle';
+import deleteX from 'assets/svg/deleteX.svg';
 import Select from '../QuestionSelect/ActivitySelect';
 import useStyles from './styles';
 
@@ -14,9 +12,10 @@ interface Props {
   index: number;
   handleValidate?: (valid: boolean, index: number) => void;
   clearValid?: (index: number) => void;
+  valid?: boolean[];
 }
 
-const QuestionList = ({ setOptionActivities, optionActivities, index, handleValidate, clearValid }: Props) => {
+const QuestionList = ({ setOptionActivities, optionActivities, index, handleValidate, clearValid, valid }: Props) => {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
 
@@ -35,7 +34,7 @@ const QuestionList = ({ setOptionActivities, optionActivities, index, handleVali
       });
       setQuestions(question);
     }
-    console.log('questions', data);
+
     // eslint-disable-next-line
   }, [data?.questions.data]);
 
@@ -67,33 +66,49 @@ const QuestionList = ({ setOptionActivities, optionActivities, index, handleVali
       setOptionActivities([[]]);
     }
   };
+  const deleteAnswer = (i: number) => {
+    const nextOptionsActivities = [...optionActivities];
+    const newValuesRow = nextOptionsActivities[index];
+    const newOptionsValues = newValuesRow.slice(0, i);
+    nextOptionsActivities[index] = newOptionsValues;
+    setOptionActivities(nextOptionsActivities);
+  };
 
   return (
     <>
-      {/* <div className={classes.questionRow}> */}
       {questions.map((question, i) => (
-        <div key={question.id} className={classNames(classes.rowActivity)}>
-          <div className={classes.selectContainer}>
-            <Select
-              index={i}
-              openActivity={openActivity}
-              onChange={(e) => handleChange(e, i)}
-              value={optionActivities && optionActivities[index][i] ? optionActivities[index][i].id : ''}
-              setOpen={setOpen}
-              open={open}
-              question={question}
-              parent={optionActivities[index]
-                .slice(0, i)
-                .map((e) => e.id)
-                .join(',')}
-            />
+        <>
+          <div key={question.id} className={classNames(classes.rowActivity)}>
+            <div className={classes.selectContainer}>
+              <Select
+                index={i}
+                openActivity={openActivity}
+                onChange={(e) => handleChange(e, i)}
+                value={optionActivities && optionActivities[index][i] ? optionActivities[index][i].id : ''}
+                setOpen={setOpen}
+                open={open}
+                question={question}
+                parent={optionActivities[index]
+                  .slice(0, i)
+                  .map((e) => e.id)
+                  .join(',')}
+              />
+            </div>
+
+            {optionActivities[index].length > i && (
+              <div className={classes.deleteContainer} onClick={() => deleteAnswer(i)}>
+                <img src={deleteX} alt="" />
+              </div>
+            )}
           </div>
-        </div>
+          {optionActivities.length > 1 &&
+            index !== optionActivities.length - 1 &&
+            ((valid && valid[index] && i === optionActivities[index].length - 1) ||
+              (valid && !valid[index] && i === optionActivities[index].length)) && (
+              <div className={classes.divider}></div>
+            )}
+        </>
       ))}
-      {(optionActivities.length > 1 || questions.length > 1) && (
-        <Remove className={classes.deleteIcon} onClick={deleteActivity} />
-      )}
-      {/* </div> */}
     </>
   );
 };
