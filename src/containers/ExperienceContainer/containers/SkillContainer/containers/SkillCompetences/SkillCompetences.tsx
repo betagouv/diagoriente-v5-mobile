16c4924e59384f50e402ba56d403084db46b6ef8
Mahdi Hellal <mahdihellal@gmail.com>
2021-04-20 08:20:08 +0100

    SkillCompetences

--- src/containers/ExperienceContainer/containers/SkillContainer/containers/SkillCompetences/SkillCompetences.tsx
+++ src/containers/ExperienceContainer/containers/SkillContainer/containers/SkillCompetences/SkillCompetences.tsx
@@ -1,20 +1,13 @@
-import React, {
- useEffect, useState, useRef, useContext,
-} from 'react';
-import { Link, RouteComponentProps } from 'react-router-dom';
+import React, { useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import { useCompetences } from 'common/requests/competences';
 import { Competence, Theme } from 'common/requests/types';
-import Grid from '@material-ui/core/Grid';
-import Slide from '@material-ui/core/Slide';
 import BreadCrumb from 'components/common/BreadCrumb/BreadCrumb';
 import Button from 'components/button/Button';
 import Spinner from 'components/SpinnerXp/Spinner';
 import Popup from 'components/common/Popup/Popup';
 import Skill from 'components/common/SkillCheckbox/SkillCheckbox';
-import useOnclickOutside from 'common/hooks/useOnclickOutside';
-import { decodeUri } from 'utils/url';
 import ValidationButton from 'components/valideButton/valideButton';
-import SelectionContext from 'common/contexts/SelectionContext';
 import useStyles from './styles';
 
 interface Props extends RouteComponentProps<{ themeId: string }> {
@@ -36,91 +29,23 @@ const ExperienceCompetence = ({
   location,
 }: Props) => {
   const classes = useStyles();
-  const refSlide = useRef(null);
-  const { open, setOpen } = useContext(SelectionContext);
-
   const { data, loading } = useCompetences({ variables: theme?.type === 'engagement' ? { type: 'engagement' } : {} });
   const [opened, setOpened] = React.useState(false);
   const [text, setText] = React.useState('');
-  const [showInfo, setShowInfo] = useState(false);
-  const [currentInfoId, setCurrentInfoId] = useState('');
-  const [currentInfoIndex, setCurrentInfoIndex] = useState(-1);
-
-  const [currentBtn, setCurrentBtn] = useState(-1);
-  const { redirect } = decodeUri(location.search);
-  const divInfo = useRef<HTMLDivElement>(null);
-  const buttonRef = useRef<HTMLButtonElement>(null);
   const [currentDescriptionIndex, setCurrentDescriptionIndex] = useState(-1);
   const [openDescription, setOpenDescription] = useState(false);
 
-  useOnclickOutside(divInfo, (e: Event) => {
-    if (
-      currentBtn >= 0
-      && document.getElementsByClassName('ignore-onclickoutside')[currentBtn].contains(e.target as any)
-    ) {
-
-    } else if (showInfo && divInfo) {
-      setShowInfo(false);
-    }
-  });
-
-  const handleShowDescription = (id: string) => {
-    const index = theme?.tooltips.findIndex((e) => e.competenceId === id);
-
-    {
-      index !== undefined && setCurrentDescriptionIndex(index);
-    }
-    setCurrentInfoId(id);
-    setShowInfo(true);
-    setOpen(false);
-  };
-
-  const handleShowInfo = (id: string) => {
-    const index = theme?.tooltips.findIndex((e) => e.competenceId === id);
-    {
-      index !== undefined && setCurrentInfoIndex(index);
-    }
-    setCurrentInfoId(id);
-    setShowInfo(true);
-    setOpen(false);
-  };
-
-  const addCompetence = (competence: Competence) => {
-    setShowInfo(false);
-    if (competences.length < 4) {
-      setCompetences([...competences, competence]);
-    } else if (competences.length === 4) {
-      setText('Tu as déjà choisi 4 compétences');
-      setOpened(true);
-    }
-  };
-
-  const deleteCompetence = (id: string) => {
-    setCompetences(competences.filter((comp) => comp.id !== id));
-  };
   const handleClose = () => {
     setOpened(false);
   };
-  const onclickBtn = () => {
-    if (competences.length === 0) {
-      setText('Tu as déjà choisi 4 compétences');
-      setOpened(true);
-    }
-  };
 
-  const isBrowser = typeof window !== 'undefined';
-  const [width, setWidth] = useState(isBrowser ? window.innerWidth : 0);
-
-  useEffect(() => {
-    window.addEventListener('resize', () => setWidth(window.innerWidth));
-  });
   const onNavigate = () => {
-    if (competences.length && competences.length <= 4) { history.push(`/experience/skill/${match.params.themeId}/competencesValues${location.search}`); }
+    if (competences.length && competences.length <= 4) {
+      history.push(`/experience/skill/${match.params.themeId}/competencesValues${location.search}`);
+    }
     setOpened(false);
   };
-  console.log('comptences', data?.competences.data);
-  console.log('comptencesSelected', competences);
-  console.log('theme?.tooltips', theme?.tooltips);
+
   return (
     <div className={classes.root}>
       <div className={classes.container}>
